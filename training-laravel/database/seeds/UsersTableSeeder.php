af6ae31f62ba2b6f7ef2327584a7175fa41fff37
takumi <takumines6@gamil.com>
2020-04-27 12:05:03 +0900

    [update]認証機能とテストユーザーの作成
    authコマンドで認証まわりを整えてテストユーザーの作成を行った

---
+++ training-laravel/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'dummy',
+            'email' => 'dummy@email.com',
+            'password' =>bcrypt('test1234'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
