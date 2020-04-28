432d6d8e646186e67af8acc840bffd608b09d598
takumi_kurogi <takumines1@gmail.com>
2020-04-28 12:02:08 +0900

    記事の詳細画面を修正した

--- training-laravel/resources/views/post/show.blade.php
+++ training-laravel/resources/views/post/show.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<button class="btn-info btn" type="button">
-                                        <a href="{{ route('post.edit',['post' => $post]) }}">編集</a>
-                                    </button>
-                                    <div>
-                                        <form action="{{ route('post.delete', ['post' => $post]) }}" method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <input class="btn btn-danger" type="submit" value="削除">
-                                        </form>
-                                    </div>
+    <div class="container">
+
+        <div class="row p-4 mb-6 bg-white rounded shadow-sm">
+            <div class="col">
+                <div class="text-center mx-auto">
+                    @if (isset($post->image))
+                        <img src="{{ $post->image }}" alt="" class="rounded border border-info ">
+                    @else
+                        <img src="{{ asset('images/no-image.jpg') }}" alt="" class="rounded border border-info">
+                    @endif
+                    <div class="">
+                        <h1 class="text-center mb-3">{{ $post->title }}</h1>
+                        <h2 class="text-center mb-3">{{ $post->body }}</h2>
+                    </div>
+                    @auth
+                        <div class="col-12 pt-3 text-center">
+                            <a class="btn btn-lg btn-primary" href="{{ route('post.edit',['post' => $post->id]) }}">編集</a>
+                            <form class="d-inline" action="{{ route('post.delete', ['post' => $post->id]) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <input type="submit" value="削除" class="btn btn-lg btn-danger">
+                            </form>
+                        </div>
+                    @endauth
+                </div>
+            </div>
+        </>
+    </div>
 @endsection
