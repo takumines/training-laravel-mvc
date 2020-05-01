c22cfae968e8669931433aab1090fe415a38a851
takumines <takumines6@gmail.com>
2020-05-01 12:41:57 +0900

    管理者が記事一覧画面と、記事詳細が見れるように修正

--- training-laravel/app/Providers/RouteServiceProvider.php
+++ training-laravel/app/Providers/RouteServiceProvider.php
@@ -23,7 +23,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/';
 
-    public const ADMIN_HOME = '/admin/home';
+    public const ADMIN_HOME = '/admin/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
